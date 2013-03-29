fb4775a44d6a19030b27ff3add6453c4ea4fac60
Draško Kokić <drasko.kokic@googlemail.com>
2013-03-29 21:30:28 +0100

    added login to the service (AIDL and implementation)

--- andlabs-lounge-lib/src/andlabs/lounge/service/LoungeService.java
+++ andlabs-lounge-lib/src/andlabs/lounge/service/LoungeService.java
@@ -2,23 +2,15 @@ package andlabs.lounge.service;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
 import android.util.Log;
 
 public class LoungeService extends Service {
 
-	private Messenger mMessenger;
-
-
 	@Override
 	public IBinder onBind(Intent intent) {
 		Log.v("LoungeService", "onBind(): arg0 = " + intent);
-		mMessenger = intent.getParcelableExtra("client-messenger");
-		return new LoungeServiceImpl(mMessenger);
+		return new LoungeServiceImpl(intent);
 	}
 
 
