303177eb32eb52864215e58f2c1bb088743df75c
Draško Kokić <drasko.kokic@googlemail.com>
2013-03-29 17:56:38 +0100

    merged back the SocketIOCallback into LoungeServiceImpl class

--- andlabs-lounge-lib/src/andlabs/lounge/service/LoungeServiceImpl.java
+++ andlabs-lounge-lib/src/andlabs/lounge/service/LoungeServiceImpl.java
@@ -1,9 +1,15 @@
 package andlabs.lounge.service;
 
+import io.socket.IOAcknowledge;
 import io.socket.IOCallback;
 import io.socket.SocketIO;
+import io.socket.SocketIOException;
 
 import java.net.MalformedURLException;
+import java.util.Arrays;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.os.Message;
@@ -16,7 +22,54 @@ public class LoungeServiceImpl extends LoungeServiceDef.Stub {
 
 	private Messenger mMessenger;
 	private SocketIO mSocketIO;
-	private IOCallback mSocketIOCallback;
+	private IOCallback mSocketIOCallback = new IOCallback() {
+
+		@Override
+		public void onMessage(JSONObject arg0, IOAcknowledge arg1) {
+			Log.d("LoungeService", String.format("IOCallback.onMessage(): arg0 = %s", arg0));
+
+		}
+
+
+		@Override
+		public void onMessage(String arg0, IOAcknowledge arg1) {
+			Log.d("LoungeService", String.format("IOCallback.onMessage(): arg0 = %s", arg0));
+
+		}
+
+
+		@Override
+		public void onError(SocketIOException arg0) {
+			Log.e("LoungeService", String.format("IOCallback.onError(): caught exception while connecting"), arg0);
+
+		}
+
+
+		@Override
+		public void onDisconnect() {
+			Log.d("LoungeService", String.format("IOCallback.onDisconnect():"));
+
+		}
+
+
+		@Override
+		public void onConnect() {
+			Log.d("LoungeService", String.format("IOCallback.onConnect():"));
+			try {
+				mSocketIO.emit("login", new JSONObject().put("playerID", "kodra"));
+			} catch (JSONException e) {
+				Log.e("LoungeService", "caught exception while sending login", e);
+			}
+		}
+
+
+		@Override
+		public void on(String arg0, IOAcknowledge arg1, Object... arg2) {
+			Log.d("LoungeService", String.format("IOCallback.on(): arg0 = %s, arg1 = %s, arg2 = %s", arg0, arg1, Arrays.toString(arg2)));
+			
+		}
+
+	};
 
 
 	public LoungeServiceImpl(Messenger pMessenger) {
@@ -39,7 +92,6 @@ public class LoungeServiceImpl extends LoungeServiceDef.Stub {
 		Log.v("LoungeService", "LoungeServiceDef.Stub.connect():");
 		try {
 			mSocketIO = new SocketIO("http://lounge-server.jit.su/");
-			mSocketIOCallback = new SocketIOCallback(mSocketIO);
 			mSocketIO.connect(mSocketIOCallback);
 		} catch (MalformedURLException e) {
 			Log.e("LoungeService", "caught exception while creating/connecting SocketIO", e);
