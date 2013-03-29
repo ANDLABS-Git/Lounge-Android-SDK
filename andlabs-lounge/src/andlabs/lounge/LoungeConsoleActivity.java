24cb335a07fdf93e6ed7a868367750ad26dd3438
Draško Kokić <drasko.kokic@googlemail.com>
2013-03-29 18:46:51 +0100

    moved android service boilerplate code into the LoungeController class

--- andlabs-lounge/src/andlabs/lounge/LoungeConsoleActivity.java
+++ andlabs-lounge/src/andlabs/lounge/LoungeConsoleActivity.java
@@ -1,46 +1,13 @@
 package andlabs.lounge;
 
-import andlabs.lounge.service.LoungeService;
-import andlabs.lounge.service.LoungeServiceDef;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.util.Log;
 import android.view.Menu;
 
 public class LoungeConsoleActivity extends Activity {
 
-	private LoungeServiceDef mLoungeService;
-
-	ServiceConnection mServiceConnection = new ServiceConnection() {
-
-		@Override
-		public void onServiceDisconnected(ComponentName name) {
-			Log.v("LoungeConsoleActivity", "ServiceConnection.onServiceDisconnected():");
-			mLoungeService = null;
-		}
-
-
-		@Override
-		public void onServiceConnected(ComponentName name, IBinder service) {
-			Log.v("LoungeConsoleActivity", "ServiceConnection.onServiceConnected():");
-			mLoungeService = LoungeServiceDef.Stub.asInterface(service);
-			try {
-				mLoungeService.connect();
-			} catch (RemoteException e) {
-				Log.e("LoungeConsoleActivity", "ServiceConnection.onServiceConnected(): caught exception while connecting", e);
-			}
-		}
-
-	};
-
+	LoungeController mLoungeController = new LoungeController();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -64,10 +31,7 @@ public class LoungeConsoleActivity extends Activity {
 		Log.v("LoungeConsoleActivity", "onStart():");
 		super.onStart();
 		// bind to the Lounge Service
-		LoungeController loungeController = new LoungeController();
-		Intent serviceIntent = new Intent(this, LoungeService.class);
-        serviceIntent.putExtra("client-messenger", loungeController.getClientMessenger());
-		bindService(serviceIntent, mServiceConnection, BIND_AUTO_CREATE);
+		mLoungeController.bindServiceTo(this);
 	}
 
 
@@ -88,14 +52,7 @@ public class LoungeConsoleActivity extends Activity {
 	@Override
 	protected void onStop() {
 		Log.v("LoungeConsoleActivity", "onStop():");
-		try {
-			if (mLoungeService != null) {
-				mLoungeService.disconnect();
-			}
-		} catch (RemoteException e) {
-			Log.e("LoungeConsoleActivity", "onPause(): caught exception while disconnecting", e);
-		}
-		unbindService(mServiceConnection);
+		mLoungeController.unbindServiceFrom(this);
 		super.onStop();
 	}
 }
--- andlabs-lounge/src/andlabs/lounge/LoungeController.java
+++ andlabs-lounge/src/andlabs/lounge/LoungeController.java
@@ -1,25 +1,77 @@
 package andlabs.lounge;
 
+import andlabs.lounge.service.LoungeService;
+import andlabs.lounge.service.LoungeServiceDef;
+import android.annotation.SuppressLint;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 
 
 public class LoungeController {
 
-	Handler handler = new Handler() {
+	@SuppressLint("HandlerLeak")
+	Messenger mMessenger = new Messenger(new Handler() {
 
 		@Override
 		public void handleMessage(Message msg) {
 			Log.v("LoungeController", "getClientMessenger(): Handler.handleMessage(): msg = " + msg);
 		}
+	});
+
+	private LoungeServiceDef mLoungeService;
+
+	ServiceConnection mServiceConnection = new ServiceConnection() {
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			Log.v("LoungeConsoleActivity", "ServiceConnection.onServiceDisconnected():");
+			mLoungeService = null;
+		}
+
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			Log.v("LoungeConsoleActivity", "ServiceConnection.onServiceConnected():");
+			mLoungeService = LoungeServiceDef.Stub.asInterface(service);
+			try {
+				mLoungeService.connect();
+			} catch (RemoteException e) {
+				Log.e("LoungeConsoleActivity", "ServiceConnection.onServiceConnected(): caught exception while connecting", e);
+			}
+		}
+
 	};
 
 
-	public Messenger getClientMessenger() {
+	public ServiceConnection getServiceConnection() {
+		return mServiceConnection;
+	}
 
-		return new Messenger(handler);
+
+	public void bindServiceTo(Context pContext) {
+		Intent serviceIntent = new Intent(pContext, LoungeService.class);
+		serviceIntent.putExtra("client-messenger", mMessenger);
+		pContext.bindService(serviceIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
+	}
+
+
+	public void unbindServiceFrom(Context pContext) {
+		try {
+			if (mLoungeService != null) {
+				mLoungeService.disconnect();
+			}
+		} catch (RemoteException e) {
+			Log.e("LoungeController", "unbindService(): caught exception while disconnecting", e);
+		}
+		pContext.unbindService(mServiceConnection);
 	}
 
 }
