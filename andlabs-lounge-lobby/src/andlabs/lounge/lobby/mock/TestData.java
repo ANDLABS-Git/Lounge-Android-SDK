10ba5f342e634ac8e0dec32fe2571700e7c63b81
Draško Kokić <drasko.kokic@googlemail.com>
2013-04-02 04:05:49 +0200

    refactored TestData class

--- andlabs-lounge-lobby/src/andlabs/lounge/lobby/mock/TestData.java
+++ andlabs-lounge-lobby/src/andlabs/lounge/lobby/mock/TestData.java
@@ -14,78 +14,88 @@ public class TestData {
     public static List<LobbyListElement> getMockData() {
 
         List<LobbyListElement> list = new ArrayList<LobbyListElement>();
-        Player pa = new Player();
-        pa.setDisplayName("Tommo");
+ 
+        Player playerA = new Player();
+        playerA.setDisplayName("Tommo");
 
-        Player pb = new Player();
-        pb.setDisplayName("Ninja");
+        Player playerB = new Player();
+        playerB.setDisplayName("Ninja");
 
-        Player pc = new Player();
-        pc.setDisplayName("Nico");
+        Player playerC = new Player();
+        playerC.setDisplayName("Nico");
 
-        LobbyListElement game1 = new LobbyListElement();
-        game1.setTitle("Mol3cool");
-        game1.setType(ElementType.JOINED_GAME);
-        game1.setPgkName("de.andlabs.gravitywins");
-        List<GameMatch> matchesGame1 = new ArrayList<GameMatch>();
-
-        GameMatch ma = new GameMatch();
-        List<Player> pl1 = new ArrayList<Player>();
-        pl1.add(pa);
-        ma.setMaxPlayers(2);
-        ma.setPlayers(pl1);
-
-        GameMatch ma2 = new GameMatch();
-        List<Player> pl2 = new ArrayList<Player>();
-        pl2.add(pb);
-        pl2.add(pa);
-        ma2.setMaxPlayers(2);
-        ma2.setPlayers(pl2);
-
-        matchesGame1.add(ma);
-        matchesGame1.add(ma2);
-        game1.setGameMatches(matchesGame1);
-
-        LobbyListElement game2 = new LobbyListElement();
-        game2.setTitle("Gravity Losses");
-        game2.setType(ElementType.JOINED_GAME);
-        game2.setPgkName("com.andlabs.gi");
-        List<GameMatch> matchesGame2 = new ArrayList<GameMatch>();
-
-        GameMatch ma3 = new GameMatch();
-        List<Player> pl3 = new ArrayList<Player>();
-        pl3.add(pa);
-        ma3.setMaxPlayers(2);
-        ma3.setPlayers(pl3);
-        matchesGame2.add(ma3);
-
-        matchesGame1.add(ma3);
-        game2.setGameMatches(matchesGame2);
-
-        LobbyListElement seperator = new LobbyListElement();
-        seperator.setType(ElementType.SEPERATOR);
-
-        LobbyListElement openGame1 = new LobbyListElement();
-        openGame1.setTitle("Mol3Cool");
-        openGame1.setPgkName("de.andlabs.gravitywins");
-        openGame1.setType(ElementType.OPEN_GAME);
-        GameMatch mb1 = new GameMatch();
-
-        mb1.setMaxPlayers(4);
-        List<Player> pl4 = new ArrayList<Player>();
-        pl4.add(pb);
-        mb1.setPlayers(pl4);
-        List<GameMatch> matchesOpen = new ArrayList<GameMatch>();
-        matchesOpen.add(mb1);
-        openGame1.setGameMatches(matchesOpen);
+        LobbyListElement game1 = createRunningGame("de.andlabs.gravitywins", "Mol3cool");
+        game1.getGameMatches().add(createGameMatch(2, playerA));
+        game1.getGameMatches().add(createGameMatch(2, playerB, playerA));
+        game1.getGameMatches().add(createGameMatch(2, playerA));
 
         list.add(game1);
+
+        LobbyListElement game2 = createRunningGame("com.andlabs.gi", "Gravity Losses");
+        game2.getGameMatches().add(createGameMatch(2, playerA));
+
         list.add(game2);
-        list.add(seperator);
-        list.add(openGame1);
+
+        list.add(createSeparator());
+
+        LobbyListElement openGame = createOpenGame("de.andlabs.gravitywins", "Mol3Cool", playerB);
+		list.add(openGame);
 
         return list;
 
     }
+
+
+	private static GameMatch createGameMatch(int pMaxPlayers, Player ... pPlayer) {
+
+		GameMatch gameMatch = new GameMatch();
+		gameMatch.setMaxPlayers(pMaxPlayers);
+		for (Player player : pPlayer) {
+			gameMatch.getPlayers().add(player);
+		}
+		return gameMatch;
+	}
+
+
+	private static LobbyListElement createRunningGame(String pPackageId, String pTitle) {
+
+		LobbyListElement runningGame = new LobbyListElement();
+
+		runningGame.setPgkName(pPackageId);
+        runningGame.setTitle(pTitle);
+        runningGame.setType(ElementType.JOINED_GAME);
+
+        return runningGame;
+	}
+
+
+	private static LobbyListElement createSeparator() {
+
+		LobbyListElement seperator = new LobbyListElement();
+		seperator.setType(ElementType.SEPERATOR);
+
+		return seperator;
+
+	}
+
+
+	private static LobbyListElement createOpenGame(String pPackageId, String pTitle, Player pPlayer) {
+
+		LobbyListElement openGame = new LobbyListElement();
+
+		openGame.setPgkName(pPackageId);
+        openGame.setTitle(pTitle);
+        openGame.setType(ElementType.OPEN_GAME);
+
+        GameMatch match = new GameMatch();
+        match.setMaxPlayers(4);
+
+        match.getPlayers().add(pPlayer);
+
+        openGame.getGameMatches().add(match);
+
+        return openGame;
+
+	}
 
 }
