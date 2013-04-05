23494dee381314e6ddc34f73f5ea775189d633b6
Johannes Borchardt <borchardt@andlabs.com>
2013-04-05 16:41:18 +0200

    Now showing _correct_ data in the open game groupviews.

--- andlabs-lounge-lobby/src/andlabs/lounge/lobby/ui/LobbyActivity.java
+++ andlabs-lounge-lobby/src/andlabs/lounge/lobby/ui/LobbyActivity.java
@@ -16,6 +16,7 @@
 package andlabs.lounge.lobby.ui;
 
 import andlabs.lounge.lobby.R;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -41,6 +42,9 @@ public class LobbyActivity extends FragmentActivity
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_lobby);
