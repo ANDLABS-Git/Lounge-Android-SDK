3d58342b04b4774fcb35c1bd8552835e86293cf7
Draško Kokić <drasko.kokic@googlemail.com>
2013-04-06 15:19:31 +0200

    added logic to handle open and joined matches when joinMatch message processed

--- andlabs-lounge-service/src/andlabs/lounge/service/LoungeMessageProcessor.java
+++ andlabs-lounge-service/src/andlabs/lounge/service/LoungeMessageProcessor.java
@@ -97,54 +97,62 @@ public abstract class LoungeMessageProcessor {
 
                 // When the game is already known, update the known data, else
                 // create a new game object.
-                final String gameID = payload.getString("gameID"); // TODO:
-                                                                   // is it
-                                                                   // called
-                                                                   // gameID or
-                                                                   // packageID?
-                                                                   // spec says
-                                                                   // packageID.
-
-                Game game = null;
-                if (involvedInMatchFlag) {
-                    game = mInvolvedGames.get(gameID);
-                } else {
-                    game = mOpenGames.get(gameID);
-                }
-                if (game == null) {
-                    game = new Game();
-                    game.gameID = gameID;
-                    game.gameName = payload.getString("gameName");
-                }
-
+                final String gameID = payload.getString("gameID");
+                Game involvedGame = mInvolvedGames.get(gameID);
+                Game openGame = mOpenGames.get(gameID);
                 final String matchID = payload.getString("matchID");
-                Match match = game.matches.get(matchID);
+                Match match = mMatches.get(matchID);
                 if (match == null) {
                     match = new Match();
                     match.matchID = matchID;
-                    game.matches.put(matchID, match);
                     mMatches.put(matchID, match);
                 }
-                match.players = new ArrayList<Player>();
-                JSONArray playerArray = payload.getJSONArray("playerIDs");
-                for (int index = 0; index < playerArray.length(); index++) {
-                    JSONObject jsonObject = playerArray.getJSONObject(index);
-
-                    String player = jsonObject.getString("_id");
-                    match.players.add(mPlayers.get(player));
-                }
-
+                match.players = players;
                 match.totalSpots = payload.getInt("totalSpots");
                 match.status = payload.getString("status");
 
-                final boolean closedFlag = "closed".equals(match.status);
-                if (!closedFlag) {
-                    if (involvedInMatchFlag) { // the user is involved
-                        mInvolvedGames.put(game.gameID, game);
-                    } else {
-                        mOpenGames.put(game.gameID, game);
+                if ("closed".equals(match.status)) {
+
+                    if (openGame != null) {
+                        openGame.matches.remove(matchID);
+                        if (openGame.matches.size() == 0) {
+                            mOpenGames.remove(gameID);
+                        }
                     }
-                } // TODO else, remove it if existing
+                    if (involvedGame != null) {
+                        involvedGame.matches.remove(matchID);
+                        if (involvedGame.matches.size() == 0) {
+                            mInvolvedGames.remove(gameID);
+                        }
+                    }
+
+                } else if (involvedInMatchFlag) {
+
+                    if (openGame != null) {
+                        openGame.matches.remove(matchID);
+                        if (openGame.matches.size() == 0) {
+                            mOpenGames.remove(gameID);
+                        }
+                    }
+                    if (involvedGame == null) {
+                        involvedGame = new Game();
+                        involvedGame.gameID = gameID;
+                        involvedGame.gameName = payload.getString("gameName");
+                        mInvolvedGames.put(gameID, involvedGame);
+                    }
+                    involvedGame.matches.put(matchID, match);
+
+                } else {
+
+                    if (openGame == null) {
+                        openGame = new Game();
+                        openGame.gameID = gameID;
+                        openGame.gameName = payload.getString("gameName");
+                        mOpenGames.put(gameID, openGame);
+                    }
+                    openGame.matches.put(matchID, match);
+
+                }
 
                 triggerUpdate(mInvolvedGames, mOpenGames);
             }
