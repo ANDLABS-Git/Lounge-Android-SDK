6310b334f359d5ff008774de49b163d4fcbce873
Lukas ANDLABS <jarosch@andlabs.eu>
2013-10-02 20:34:15 +0200

    removed  unrequired manifest entries in lounge games because of manitest merger.
    Todo: GET_ACCOUNTS Permission still needs to be removed as a requirement.

---
+++ andlabs-lounge-debugger/src/andlabs/loungedebugger/StartActivity.java
@@ -0,0 +1,25 @@
+package andlabs.loungedebugger;
+
+import andlabs.lounge.lobby.ui.LoungeActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class StartActivity extends Activity {
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_activity);
+        
+    }
+
+   
+    
+    public void openLobby(View v){
+     startActivity(new Intent(this,LoungeActivity.class));   
+    }
+}
