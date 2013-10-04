43ff9c003976a0132dac6b85f1d33a21f71935ff
Lukas ANDLABS <jarosch@andlabs.eu>
2013-10-04 17:20:05 +0200

    Added some first basic UI Logic for Login

--- andlabs-lounge-lobby/src/andlabs/lounge/lobby/ui/LoginActivity.java
+++ andlabs-lounge-lobby/src/andlabs/lounge/lobby/ui/LoginActivity.java
@@ -1,12 +1,27 @@
 package andlabs.lounge.lobby.ui;
 
+import java.util.Random;
+
 import andlabs.lounge.lobby.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class LoginActivity extends Activity {
+
+
+    private static final String PLAYER_NAME = "PLAYER_NAME";
+    private static final String USER_ID = "USER_ID";
+    private TextView description;
+    private View usernameLayout;
+    private EditText userNameField;
+    private Button playNowBtn;
+    private boolean inTempLoginState;
 
 
     @Override
@@ -14,10 +29,28 @@ public class LoginActivity extends Activity {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        description = (TextView) findViewById(R.id.description);
+        usernameLayout = findViewById(R.id.usernameLayout);
+        userNameField = (EditText) findViewById(R.id.userNameField);
+        playNowBtn = (Button) findViewById(R.id.playNowBtn);
     }
 
 
     public void playnow(View v) {
+
+        if (!inTempLoginState) {
+            inTempLoginState = true;
+            playNowBtn.setText("Join Lobby");
+            description.setVisibility(View.GONE);
+            usernameLayout.setVisibility(View.VISIBLE);
+        } else {
+            Intent i=new Intent(this,LoungeActivity.class);
+            i.putExtra(PLAYER_NAME, userNameField.getText().toString());
+            i.putExtra(USER_ID, "TEMP_"+new Random().nextLong());
+            
+            
+        }
+
 
     }
 
