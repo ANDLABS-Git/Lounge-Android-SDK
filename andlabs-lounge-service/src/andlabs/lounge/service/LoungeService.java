a180927df9afccea4bd974926f3165e4c9242342
Lukas Jarosch <jarosch@andlabs.com>
2013-10-31 01:14:14 +0100

    added first foreground service notification Button works (prints debug msg) Textfield only work the first time, dont update yet.

--- andlabs-lounge-service/src/andlabs/lounge/service/LoungeService.java
+++ andlabs-lounge-service/src/andlabs/lounge/service/LoungeService.java
@@ -26,10 +26,14 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.accounts.OnAccountsUpdateListener;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.IBinder;
@@ -37,15 +41,24 @@ import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
 import android.provider.Settings.Secure;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat.Builder;
+import android.util.Log;
+import android.widget.RemoteViews;
 
 public class LoungeService extends Service {
+
+    private static final String SWITCH_EVENT = "CLICK";
 
     private Messenger mMessenger;
 
     private LoungeServiceImpl mLoungeService = new LoungeServiceImpl() {
 
+        private String playerId = "playerid";
+        private String playerName = "playerName";
+
+
         public String getPlayerId() {
-            String playerId = null;
             String packageName = LoungeService.this.getPackageName();
             if ("andlabs.lounge.app".equalsIgnoreCase(packageName)) {
                 AccountManager accountManager = (AccountManager) LoungeService.this.getSystemService(Activity.ACCOUNT_SERVICE);
@@ -55,7 +68,8 @@ public class LoungeService extends Service {
                     Ln.d("playerId = %s", playerId);
                 } else {
                     Ln.d("here could a notification ask user to login/register");
-//                    this.startActivity(new Intent("andlabs.lounge.account.AUTHENTICATE"));
+                    // this.startActivity(new
+                    // Intent("andlabs.lounge.account.AUTHENTICATE"));
                 }
             } else {
                 playerId = Secure.getString(LoungeService.this.getContentResolver(), Secure.ANDROID_ID);
@@ -64,8 +78,8 @@ public class LoungeService extends Service {
             return playerId;
         };
 
+
         public String getPlayerName() {
-            String playerName = null;
             String packageName = LoungeService.this.getPackageName();
             if ("andlabs.lounge.app".equalsIgnoreCase(packageName)) {
                 AccountManager accountManager = (AccountManager) LoungeService.this.getSystemService(Activity.ACCOUNT_SERVICE);
@@ -75,7 +89,8 @@ public class LoungeService extends Service {
                     Ln.d("playerName = %s", playerName);
                 } else {
                     Ln.d("here could a notification ask user to login/register");
-//                    this.startActivity(new Intent("andlabs.lounge.account.AUTHENTICATE"));
+                    // this.startActivity(new
+                    // Intent("andlabs.lounge.account.AUTHENTICATE"));
                 }
             } else {
                 playerName = getPlayerId().substring(0, 5).toUpperCase(Locale.UK);
@@ -83,6 +98,7 @@ public class LoungeService extends Service {
             }
             return playerName;
         };
+
 
         @Override
         public void login(String uuid, String playerId) {
@@ -93,12 +109,15 @@ public class LoungeService extends Service {
     };
 
     private LoungeServiceImpl.MessageHandler mMessageHandler = new LoungeServiceImpl.MessageHandler() {
-        
+
         @Override
         public void send(Message message) {
             try {
                 if (mMessenger != null) {
                     mMessenger.send(message);
+                    if (message != null && message.obj != null) {
+                        updateNotification(message.what + "", message.arg1 + "", message.obj.toString());
+                    }
                 } else {
                     Ln.w("mMessanger is not set, throwing %s", message);
                 }
@@ -109,7 +128,7 @@ public class LoungeService extends Service {
     };
 
     private OnAccountsUpdateListener mOnAccountsUpdateListener = new OnAccountsUpdateListener() {
-        
+
         @Override
         public void onAccountsUpdated(Account[] accounts) {
             String packageName = LoungeService.this.getPackageName();
@@ -120,12 +139,17 @@ public class LoungeService extends Service {
                     mLoungeService.login(null, null);
                 } else {
                     Ln.d("here could a notification ask user to login/register");
-//                    this.startActivity(new Intent("andlabs.lounge.account.AUTHENTICATE"));
+                    // this.startActivity(new
+                    // Intent("andlabs.lounge.account.AUTHENTICATE"));
                 }
             }
         }
 
     };
+
+    private SwitchButtonListener switchButtonListener;
+
+    private Builder builder;
 
 
     @Override
@@ -135,12 +159,14 @@ public class LoungeService extends Service {
         return mLoungeService;
     }
 
+
     @Override
     public boolean onUnbind(Intent intent) {
         Ln.v("intent = %s", intent);
         mMessenger = null;
         return super.onUnbind(intent);
     }
+
 
     @Override
     public void onCreate() {
@@ -150,6 +176,75 @@ public class LoungeService extends Service {
         mLoungeService.connect();
         AccountManager accountManager = (AccountManager) LoungeService.this.getSystemService(Activity.ACCOUNT_SERVICE);
         accountManager.addOnAccountsUpdatedListener(mOnAccountsUpdateListener, null, true);
+        // this.setForeground(true);
+        startForeground(1, getCustomNotification("init service", "-", "-"));
+    }
+
+
+    public Notification getCustomNotification(String n1, String n2, String n3) {
+        // Using RemoteViews to bind custom layouts into Notification
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.notification);
+
+        // Open NotificationView Class on Notification Click
+        // Send data to NotificationView Class
+        // Open NotificationView.java Activity
+
+        Intent intent = new Intent();
+        // Send data to NotificationView Class
+        // Open NotificationView.java Activity
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        if (builder == null) {
+            builder = new NotificationCompat.Builder(this)
+            // Set Icon
+                    .setSmallIcon(R.drawable.lounge_icon).setOnlyAlertOnce(true)
+                    // Set Ticker Message
+                    .setTicker("Lounge Debug")
+                    // Dismiss Notification
+                    .setAutoCancel(true).setContentIntent(pIntent)
+                    // Set PendingIntent into Notification
+                    // Set RemoteViews into Notification
+                    .setContent(remoteViews);
+        }
+
+        // Locate and set the Image into customnotificationtext.xml ImageViews
+        // remoteViews.setImageViewResource(R.id.imagenotileft,R.drawable.lounge_icon);
+        // remoteViews.setImageViewResource(R.id.imagenotiright,R.drawable.lounge_icon);
+
+        // Locate and set the Text into customnotificationtext.xml TextViews
+        remoteViews.setTextViewText(R.id.state1, n1);
+        remoteViews.setTextViewText(R.id.state2, n2);
+        remoteViews.setTextViewText(R.id.state3, n3);
+
+        Intent switchIntent = new Intent(SWITCH_EVENT);
+        PendingIntent pendingSwitchIntent = PendingIntent.getBroadcast(this, 0, switchIntent, 0);
+
+        remoteViews.setOnClickPendingIntent(R.id.kill_service, pendingSwitchIntent);
+
+        switchButtonListener = new SwitchButtonListener();
+        registerReceiver(switchButtonListener, new IntentFilter(SWITCH_EVENT));
+
+        // Create Notification Manager
+        // Build Notification with Notification Manager
+        // notificationmanager.notify(0, builder.build());
+        Log.i("debug", "notif " + builder.build());
+        return builder.build();
+
+    }
+
+
+    public void updateNotification(String n1, String n2, String n3) {
+        startForeground(1, getCustomNotification(n1, n2, n3));
+    }
+
+
+    public class SwitchButtonListener extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("debug", "buttttonn");
+        }
+
     }
 
 
@@ -166,9 +261,10 @@ public class LoungeService extends Service {
         return super.onStartCommand(intent, flags, startId);
     }
 
+
     @Override
     public void onDestroy() {
-        Ln.v("onDestroy():"); 
+        Ln.v("onDestroy():");
         if (mLoungeService != null) {
             mLoungeService.disconnect();
         }
@@ -176,7 +272,6 @@ public class LoungeService extends Service {
         accountManager.removeOnAccountsUpdatedListener(mOnAccountsUpdateListener);
         super.onDestroy();
     }
-
 
     private boolean mRetry = false;
 
@@ -194,13 +289,15 @@ public class LoungeService extends Service {
                     .getParcelableExtra(ConnectivityManager.EXTRA_OTHER_NETWORK_INFO);
 
             if (noConnectivity) {
-                mRetry = true; // if there's no connection, try to reconnect once the connection is established again
+                mRetry = true; // if there's no connection, try to reconnect
+                               // once the connection is established again
             } else {
                 tryReconnect();
             }
         }
 
     };
+
 
     private void tryReconnect() {
         if (mRetry) {
